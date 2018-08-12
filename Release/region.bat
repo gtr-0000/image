31a6fa2f5cd0f7a46c08394d30d111710d88e256
gtr-0000 <1195132772@qq.com>
2018-08-12 17:01:07 +0800

    release

--- Release/region.bat
+++ Release/region.bat
@@ -4,21 +4,32 @@ if "%image%"=="size cmd" "%~dp0cmd.exe" /c "%~f0"&goto:eof
 
 setlocal enabledelayedexpansion
 set image=resize cmd 500 500
-set image=newbuf r 200 200 128 0 0
-set image=newbuf g 100 100 0 128 0
-set image=newbuf b 50 50 0 0 128
+set image=newbuf red 200 200 128 0 0
+set image=newbuf green 100 100 0 128 0
+set image=newbuf blue 50 50 0 0 128
+set image=newbuf purple 150 150 128 0 128
+
+set image=target purple
+set image=draw green 0 0
+set image=draw blue 100 0
+set image=draw red 75 25
+set image=target cmd
+
 set image=cls
 pause
-set image=draw r 100 100
+set image=draw red 100 100
 set image=debug
 pause
-set image=draw g 150 150
+set image=draw green 150 150
 set image=debug
 pause
-set image=draw b 175 225
+set image=draw blue 175 225
+set image=debug
+pause
+set image=draw purple 300 300
 set image=debug
 echo 请点击鼠标. . . 
 for /l %%# in () do (
 	set image=mouse -1
-	title !image!
+	title 你点击了!imagepic!
 )
